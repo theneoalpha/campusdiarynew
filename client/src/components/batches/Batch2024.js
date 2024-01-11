e562435c0f131d617bc829d10847c9f2ed50ca38
theneoalpha <vikashkaushik910@gmail.com>
2024-01-12 03:44:12 +0530

    FirstCommit

---
+++ client/src/components/batches/Batch2024.js
@@ -0,0 +1,325 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import boy from "./image/boy.jpg";
+import girl from "./image/girl.jpg";
+import "../assets/cards.css";
+import "./batch.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+// import YouTubeIcon from '@mui/icons-material/YouTube';
+
+export default function Batch2024() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="batch_container"> <h1> Batch 2020-24</h1>
+        <div className="row">
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team ">
+              <div className="picture">
+                <img className="img-fluid" src="https://scontent.cdninstagram.com/v/t51.2885-19/410200186_366624569188461_7624045907190699982_n.jpg?stp=dst-jpg_s120x120&_nc_cat=111&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=MhYchIDBpkgAX-vvnCY&_nc_ht=scontent.cdninstagram.com&oh=00_AfCvbUwMMaP5hB0K18f1hebY2qdC9-WEUbTE1u176ipkMA&oe=65A5F228" />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Vikash Kaushik</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="https://www.instagram.com/theneoalpha/">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://github.com/theneoalpha">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/vikashkaushik79/">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="https://twitter.com/i/flow/login?redirect_after_login=%2Fvikashkaushik79">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src="https://scontent.cdninstagram.com/v/t51.2885-19/411326830_1833869633713574_6000907943331843875_n.jpg?stp=dst-jpg_s120x120&_nc_cat=100&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=rAh3rDPLLV4AX-LgsQF&_nc_ht=scontent.cdninstagram.com&oh=00_AfB7vD1gwMrJaP89fXnbOQZNztMRIzi_r8G3bZ2735wnFA&oe=65A5D6C9" />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Avinash Singh</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="https://www.instagram.com/avinash_fitz?igsh=cXdwM2J3MGR3c2dw&utm_source=qr">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src="https://scontent.cdninstagram.com/v/t51.2885-19/417458204_739762701055289_483173472118831536_n.jpg?stp=dst-jpg_s120x120&_nc_cat=108&ccb=1-7&_nc_sid=c4dd86&_nc_ohc=de1P_6e3U3AAX8tudEU&_nc_ht=scontent.cdninstagram.com&oh=00_AfDehO4a9_LE2ryJDfCeVb660Oq6WscaDhzsk3HldQ6bUg&oe=65A4E1AD" />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Tikesh Usendi</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="https://www.instagram.com/itst_ikesh?igsh=NzYzYzVsaTZmcTJk">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={boy} />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Tushar Sahu</h3>
+                <h4 className="title">Batch 2020-24</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="#">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={boy} />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Derhu Joshi</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="#">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={boy} />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Dooman Maitry</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="#">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={boy} />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Ajay</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="#">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={boy} />
+              </div>
+              <div className="team-content">
+                <h3 className="name">Neetish Yadu</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="#">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={boy} />
+              </div>
+              <div className="team-content">
+                <h3 className="name" />
+                <h3>Aayush Verma</h3>
+                <h4 className="title">Batch 2020-24(IT)</h4>
+              </div>
+              <ul className="social">
+                <li>
+                  <a href="#">
+                    <InstagramIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <GitHubIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <LinkedInIcon />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <EmailIcon />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
